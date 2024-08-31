6414012aea8a5505de1f5f76191747a97bf5e05b
LUPRWEST <97930656+LUPRWEST@users.noreply.github.com>
2024-09-01 01:49:33 +0900

    Add files via upload

--- musicplayer.js
+++ musicplayer.js
@@ -1,36 +1,20 @@
-player.onmousedown = function(event) {
+const imageContainer = document.getElementById('image-container');
+const imageElement = document.getElementById('player');
+const audioElement = document.getElementById('audio');
+    
+let isPlaying = false;
+    
+  imageContainer.addEventListener('click', function() {
+  if (isPlaying) {
+    // 원래 이미지로 돌아오고 오디오를 멈춤
+    imageElement.src = 'https://github.com/LUPRWEST/luprwest.github.io/blob/main/music_off.PNG?raw=true';
+    audioElement.pause();
+    audioElement.currentTime = 0;
+    } else {
+      // 다른 이미지로 바뀌고 오디오 재생
+      imageElement.src = 'https://github.com/LUPRWEST/luprwest.github.io/blob/main/music_on.PNG?raw=true';
+       audioElement.play();
+  }
+  isPlaying = !isPlaying;
+});
 
-    let shiftX = event.clientX - player.getBoundingClientRect().left;
-    let shiftY = event.clientY - player.getBoundingClientRect().top;
-  
-    player.style.position = 'absolute';
-    player.style.zIndex = 1000;
-    document.body.append(player);
-  
-    moveAt(event.pageX, event.pageY);
-  
-    // 초기 이동을 고려한 좌표 (pageX, pageY)에서
-    // 플레이어을 이동.
-    function moveAt(pageX, pageY) {
-      player.style.left = pageX - shiftX + 'px';
-      player.style.top = pageY - shiftY + 'px';
-    }
-  
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-    }
-  
-    // mousemove로 플레이어을 움직임.
-    document.addEventListener('mousemove', onMouseMove);
-  
-    // 플레이어을 드롭하고, 불필요한 핸들러를 제거.
-    player.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      player.onmouseup = null;
-    };
-  
-  };
-  
-  player.ondragstart = function() {
-    return false;
-  };
